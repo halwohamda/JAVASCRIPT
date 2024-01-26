5754c0ffd4197fe856f0e2fe6926a884a4f0eb9a
halwohamda <halwohamda@gmail.com>
2024-01-26 09:00:05 -0800

    done

--- week1/Day1/object.js
+++ week1/Day1/object.js
@@ -5,3 +5,14 @@ let Person={
     phone: "095348748"
 }
 console.log(id.length)
+
+
+let Students=[
+    {
+        id:1,
+    name: "halwo",
+    address: "10aad",
+    phone: "095348748"
+    },
+];
+console.log(Students);
