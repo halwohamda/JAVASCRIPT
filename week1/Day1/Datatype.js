926532bd2146665540ac177fd333cb281dd5f050
halwohamda <halwohamda@gmail.com>
2024-01-28 00:11:24 -0800

    done

--- week1/Day1/Datatype.js
+++ week1/Day1/Datatype.js
@@ -1,5 +1,11 @@
-let message = "hello halwo";
+  // Type string
+ let message = "hello halwo";
 console.log(typeof message);
 
+// Type Number
 let number = 30;
 console.log(typeof number);
+
+//Boolen
+let Admin = true;
+console.log(Admin);
