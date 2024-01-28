c418d02cf207b3c2d3951cadcc0089841ba113c1
halwohamda <halwohamda@gmail.com>
2024-01-28 00:03:21 -0800

    done

--- week1/Day1/variables.js
+++ week1/Day1/variables.js
@@ -8,3 +8,20 @@ let firstName = "halwo";
 let middleName = "adam";
 let fullName =`${firstName} ${middleName}`
 console.log(fullName);
+
+
+
+//Declarative of variable
+
+
+let name1 = "how are you";
+console.log(name1);
+
+let name2 = "Iam fine";
+console.log(name2);
+
+let name3 = "what's new";
+console.log(name3)
+
+let name4 = "Nothing man";
+console.log(name4);
