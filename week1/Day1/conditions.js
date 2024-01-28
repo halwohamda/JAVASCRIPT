7f67b587ed79cdb18a2a0e4e446c10ce4fd070cf
halwohamda <halwohamda@gmail.com>
2024-01-27 22:01:23 -0800

    done

--- week1/Day1/conditions.js
+++ week1/Day1/conditions.js
@@ -1,3 +1,16 @@
-let Password = 12345
-let result = Password <12345? "fadlan wad khaladay" : " waad saxday "
-console.log(result)
+
+// //  TERINARY operator
+// let Password = 12345
+// let result = Password <234567? "fadlan wad khaladay" : " waad saxday "
+// console.log(result)
+
+
+
+// IF AND ELSE
+let name = "halwo";
+
+if(name == 'halwo'){
+    console.log("soo dhowow  marwo: "+name)
+}else{
+    console.log("soodhawoow mudane : "+name)
+}
--- week1/Day1/object.js
+++ week1/Day1/object.js
@@ -4,31 +4,31 @@ let Person={
     address: "10aad",
     phone: "095348748"
 }
-console.log(id.length)
+console.log(Person)
 
 
-let Students=[
-    {
-        id:1,
-    name: "halwo",
-    address: "10aad",
-    phone: "095348748"
-    },
-    {
-        id:1,
-    name: "halwo",
-    address: "10aad",
-    phone: "095348748"
-    },{
-        id:1,
-    name: "halwo",
-    address: "10aad",
-    phone: "095348748"
-    },{
-        id:1,
-    name: "halwo",
-    address: "10aad",
-    phone: "095348748"
-    },
-];
-console.log(Students);
+// let Students=[
+//     {
+//         id:1,
+//     name: "halwo",
+//     address: "10aad",
+//     phone: "095348748"
+//     },
+//     {
+//         id:1,
+//     name: "halwo",
+//     address: "10aad",
+//     phone: "095348748"
+//     },{
+//         id:1,
+//     name: "halwo",
+//     address: "10aad",
+//     phone: "095348748"
+//     },{
+//         id:1,
+//     name: "halwo",
+//     address: "10aad",
+//     phone: "095348748"
+//     },
+// ];
+// console.log(Students);
