f46d2363b0bd3f6adffdb38c1feb59e9c6ae1d7e
halwohamda <halwohamda@gmail.com>
2024-01-28 06:18:41 -0800

    done

--- week1/Day1/Operations.js
+++ week1/Day1/Operations.js
@@ -1,3 +1,46 @@
 // Addition
 console.log(4+4+3)
 
+// Substraction
+console.log(7-8-3)
+
+// Multiplication
+console.log(4*5);
+
+
+//Division
+console.log(32/2);
+
+// Logical Operator
+// And operator
+let number1 = 10;
+let number2 = 20;
+let result =number1<number2 && number1>number2
+console.log(`natiijada xisabtani waa : ${result}`);
+
+// Or Operator
+
+let number = 10;
+let number0 = 20;
+let result1 =number<number0 || number>number0
+console.log(`natiijada xisabtani waa : ${result1}`);
+
+// Not operator
+let number6 = 10;
+let number4 = 20;
+let result0 = !number6>number4
+console.log(`natiijada xisabtani waa : ${result0}`);
+
+// Less than
+
+// let less1 = 6;
+// let less2 = 5;
+// let result9 = less1<less2
+// console.log(result9);
+
+
+// // Greater tham
+// let less11 = 6;
+// let less22 = 5;
+// let result99 = less11>less22
+// console.log(result99);
