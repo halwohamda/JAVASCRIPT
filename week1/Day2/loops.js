d6fedf93f1e33f0011e9451db9548508ab357ea1
halwohamda <halwohamda@gmail.com>
2024-01-29 07:09:49 -0800

    done

--- week1/Day2/loops.js
+++ week1/Day2/loops.js
@@ -2,7 +2,7 @@ for(let i=0; i<=10; i++);
 // console.log(i);
 
 
-let items = ["bisad","mas","saliid","subag"]
+let items = ["bisad","mas","saliid","subag"];
 for(i=3; i<items.length; i++){
-    console.log(items[i])
+    console.log(items[i]);
 }
