e6d097be1f82630e5daceed5b1d794e6f2e1bf2c
halwohamda <halwohamda@gmail.com>
2024-01-29 05:08:30 -0800

    done

--- week1/Day1/Operations.js
+++ week1/Day1/Operations.js
@@ -71,5 +71,5 @@ console.log(result79);
 
 let notEqual = 9;
 let notequal = 5;
-let resultt = notEqual !== notequal
-console.log(resultt)
+let resultt = notEqual !== notequal;
+console.log(resultt);
