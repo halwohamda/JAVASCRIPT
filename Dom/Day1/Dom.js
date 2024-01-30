a10badb67d416f71289516f50ce881f2e14ea799
halwohamda <halwohamda@gmail.com>
2024-01-30 07:10:40 -0800

    done

--- Dom/Day1/Dom.js
+++ Dom/Day1/Dom.js
@@ -1,5 +1,5 @@
 // by tagName
-document.querySelector('h1').innerText = "hello xamar";
+document.querySelector('h1').innerText = "hello xamar🚈";
 
 let hello = document.querySelector('h1');
 hello.innerText = 'hello hargaysa';
