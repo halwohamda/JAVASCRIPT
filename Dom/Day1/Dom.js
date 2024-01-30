384d3e2c1fe0c65d45f04c92612eb318a7c7857b
halwohamda <halwohamda@gmail.com>
2024-01-30 06:53:56 -0800

    done

--- Dom/Day1/Dom.js
+++ Dom/Day1/Dom.js
@@ -4,3 +4,6 @@ document.querySelector('h1').innerText = "hello xamar";
 let hello = document.querySelector('h1');
 hello.innerText = 'hello hargaysa';
 
+// by Id
+
+document.getElementById('car').innerText = 'gaadhi'
