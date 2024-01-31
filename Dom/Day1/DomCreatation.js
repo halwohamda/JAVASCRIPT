e9cc0efbd6c21bb54ffe587d57af94a2a0709d87
halwohamda <halwohamda@gmail.com>
2024-01-31 06:22:41 -0800

    done

---
+++ Dom/Day1/DomCreatation.js
@@ -0,0 +1,8 @@
+ let body = document.querySelector('body');
+let message = document.createElement('p');
+
+
+message.innerText = "hello mss esra"
+body.appendChild(message);
+
+message.style.fontSize = '60px';
