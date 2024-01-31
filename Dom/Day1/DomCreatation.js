7b0bc11d986e296b17583a7fd758a8ad8d0c07aa
halwohamda <halwohamda@gmail.com>
2024-01-31 06:26:45 -0800

    done

--- Dom/Day1/DomCreatation.js
+++ Dom/Day1/DomCreatation.js
@@ -6,3 +6,13 @@ message.innerText = "hello mss esra"
 body.appendChild(message);
 
 message.style.fontSize = '60px';
+
+
+let myMessage = `
+backgroun-color : orange;
+padding : 20px;
+font-weight : 20px;
+margin : auto
+
+`
+ message.style.myMessage
