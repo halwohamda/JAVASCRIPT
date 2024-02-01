286a87c9963cab6babd0c99ad758f402c06dad4e
halwohamda <halwohamda@gmail.com>
2024-02-01 06:03:57 -0800

    done

--- Dom/Day1/DomCreatation.js
+++ Dom/Day1/DomCreatation.js
@@ -7,12 +7,14 @@ body.appendChild(message);
 
 message.style.fontSize = '60px';
 
+message.style.backgroundColor = 'orange';
+message.style.color = 'white';
+message.style.padding = '100px';
+message.style.width = '100px';
+message.style.height = '100px';
+message.style.borderRadius = '50%';
+message.style.textAlign = 'center';
+message.style.text = '2px 2px 20px ';
 
-let myMessage = `
-backgroun-color : orange;
-padding : 20px;
-font-weight : 20px;
-margin : auto
 
-`
- message.style.myMessage
+
