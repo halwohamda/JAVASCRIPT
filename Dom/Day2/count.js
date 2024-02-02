b2526638ae4a697b3db8b12730a288619f2706e8
halwohamda <halwohamda@gmail.com>
2024-02-02 07:48:54 -0800

    done

--- Dom/Day2/count.js
+++ Dom/Day2/count.js
@@ -1,8 +1,9 @@
-let count = document.getElementById('count');
-let counter = 0;
-
-
+let count = document.getElementById('count')
+let counter = 0
 
 function Add(){
     count.innerHTML = counter++
+}
+function add(){
+    count.innerHTML = counter --
 }
