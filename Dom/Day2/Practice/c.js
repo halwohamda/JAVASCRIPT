99da3514e0420b4d9c46dea846f2216c2d83ce49
halwohamda <halwohamda@gmail.com>
2024-02-03 09:25:30 -0800

    done

--- Dom/Day2/Practice/c.js
+++ Dom/Day2/Practice/c.js
@@ -4,11 +4,11 @@ let Timer = document.getElementById("Timer")
 
 // let timer
 
-// start.addEventListener('click', ()=>{
-//     let seconds = 0
-//     timer=setInterval(() => {
-//         // Timer.innerHTML = Math.random(+seconds)*100
-//         Timer.innerHTML = +seconds
-//         seconds++
-//     }, 10);
-// })
+start.addEventListener('click', ()=>{
+    let seconds = 0
+    timer=setInterval(() => {
+        // Timer.innerHTML = Math.random(+seconds)*100
+        Timer.innerHTML = +seconds
+        seconds++
+    }, 10);
+})
