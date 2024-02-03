986d366267f0a7ecf1a4241f1a0845640138f3d8
halwohamda <halwohamda@gmail.com>
2024-02-03 09:24:20 -0800

    done

--- Dom/Day2/Practice/p.js
+++ Dom/Day2/Practice/p.js
@@ -2,18 +2,18 @@
 // let InputValue = document.getElementById('InputValue');
 // let div = document.querySelector('div');
 
-// form.addEventListener('click',(e)=>{
-//      e.preventDefault();
-//      if(InputValue.value === ''){
-//         alert('Please Enter Information')
-//      }else{
-//         InputValue.value
-//         let p = document.createElement('p')
-//         p.innerHTML = InputValue.value
-//         div.appendChild(p)
-//         InputValue.value = '';
-//      }
+form.addEventListener('click',(e)=>{
+     e.preventDefault();
+     if(InputValue.value === ''){
+        alert('Please Enter Information')
+     }else{
+        InputValue.value
+        let p = document.createElement('p')
+        p.innerHTML = InputValue.value
+        div.appendChild(p)
+        InputValue.value = '';
+     }
  
 
-// });
+});
 
