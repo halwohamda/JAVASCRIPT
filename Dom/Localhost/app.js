aa5ca3533baf8835fa06c1d93f10490d21339bd5
halwohamda <halwohamda@gmail.com>
2024-02-05 07:29:02 -0800

    h

--- Dom/Localhost/app.js
+++ Dom/Localhost/app.js
@@ -21,7 +21,7 @@ btnDeleted.addEventListener('click',()=>{
 
 const savedData = localStorage.getItem("data");
 if(savedData){
-    const p = document.createElement("p");
+    const p = document.createElement("p")
     p.innerHTML = savedData
     dataList.appendChild(p)
 };
