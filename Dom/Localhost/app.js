e04f5b0f577d9a5854e235c4cf0b3b4ef72e8cb8
halwohamda <halwohamda@gmail.com>
2024-02-05 07:40:45 -0800

    h

--- Dom/Localhost/app.js
+++ Dom/Localhost/app.js
@@ -5,9 +5,9 @@ const btnDeleted = document.getElementById("btnDeleted");
 
 
 
-// btnAdd.addEventListener('click',()=>{
-//     const data = inputTask.value
-//     localStorage.setItem("data",data);
+btnAdd.addEventListener('click',()=>{
+    const data = inputTask.value
+    localStorage.setItem("data",data);
 //     const p = document.createElement("p");
 //     p.innerHTML = data;
 //     dataList.appendChild(p);
