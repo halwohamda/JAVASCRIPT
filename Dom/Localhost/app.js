b6f18eef22ad92df4576a8924a852070d4d30b7a
halwohamda <halwohamda@gmail.com>
2024-02-05 07:31:56 -0800

    h

--- Dom/Localhost/app.js
+++ Dom/Localhost/app.js
@@ -1,4 +1,4 @@
-// const inputTask = document.getElementById("inputTask");
+const inputTask = document.getElementById("inputTask");
 const btnAdd = document.getElementById("btnAdd");
 const dataList = document.getElementById("dataList");
 const btnDeleted = document.getElementById("btnDeleted");
