6771f112cbda2b5f3420284690ef0671138dfc8a
halwohamda <halwohamda@gmail.com>
2024-02-05 07:28:22 -0800

    h

--- Dom/Localhost/app.js
+++ Dom/Localhost/app.js
@@ -7,7 +7,7 @@ const btnDeleted = document.getElementById("btnDeleted");
 
 btnAdd.addEventListener('click',()=>{
     const data = inputTask.value
-    localStorage.setItem("data",data)
+    localStorage.setItem("data",data);
     const p = document.createElement("p");
     p.innerHTML = data;
     dataList.appendChild(p);
