8743ef6f9205bfa024f9203de5213e90d82694bb
halwohamda <halwohamda@gmail.com>
2024-02-06 07:49:11 -0800

    done

--- revision/ap.js
+++ revision/ap.js
@@ -1,2 +1,4 @@
 let input = document.querySelector('input');
 let btnSubmit = document.getElementById('Submit');
+let btnDelete = document.getElementById('Delete')
+let p = document.querySelector('p');
