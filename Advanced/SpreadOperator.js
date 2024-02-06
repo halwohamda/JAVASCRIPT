3993d0b613042b432fa8d11a34837ff5707b4bab
halwohamda <halwohamda@gmail.com>
2024-02-06 06:52:48 -0800

    done

--- Advanced/SpreadOperator.js
+++ Advanced/SpreadOperator.js
@@ -5,3 +5,13 @@ let items = ()=>{
     return [...Item,NewItem]
 }
 console.log(items())
+
+
+let person = [
+    {
+        id:1,
+        name:'isra',
+        phone:'987654'
+    }
+]
+console.log
