781ef671d67769519e40f81d9059f1d5a80b62b9
halwohamda <halwohamda@gmail.com>
2024-02-06 07:50:16 -0800

    done

--- revision/ap.js
+++ revision/ap.js
@@ -2,3 +2,10 @@ let input = document.querySelector('input');
 let btnSubmit = document.getElementById('Submit');
 let btnDelete = document.getElementById('Delete')
 let p = document.querySelector('p');
+
+
+btnSubmit.addEventListener('click',()=>{
+    const getData = input.value
+    p.innerHTML=getData
+    
+})
