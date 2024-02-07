9aa48a31bfc92147dfa3b075f23d69ece1f9c050
halwohamda <halwohamda@gmail.com>
2024-02-07 06:58:32 -0800

    done

--- Advanced/class.js
+++ Advanced/class.js
@@ -21,5 +21,5 @@ const Hospital6 = new Hospital(1, 'HAMDA','adam','10aad',new Date())
 const Hospital7 = new Hospital(1, 'HAMDA','adam','10aad',new Date())
 const Hospital8 = new Hospital(1, 'HAMDA','adam','10aad',new Date())
 const Hospital9 = new Hospital(1, 'HAMDA','adam','10aad',new Date())
-console.log(Hospital1,Hospita2,Hospital3,Hospita3,Hospital4,Hospital5,Hospital6,Hospital7,Hospital8)
+console.log(Hospital1,Hospital2,Hospital3,Hospital3,Hospital4,Hospital5,Hospital6,Hospital7,Hospital8)
 
