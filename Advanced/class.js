793e45997c3468d5e819b2cde7a380c133fa5cad
halwohamda <halwohamda@gmail.com>
2024-02-07 06:57:46 -0800

    done

--- Advanced/class.js
+++ Advanced/class.js
@@ -13,4 +13,13 @@ class Hospital {
 }
 
 const Hospital1 = new Hospital(1, 'HAMDA','adam','10aad',new Date())
-console.log(Hospital)
+const Hospital2 = new Hospital(1, 'HAMDA','adam','10aad',new Date())
+const Hospital3 = new Hospital(1, 'HAMDA','adam','10aad',new Date())
+const Hospital4 = new Hospital(1, 'HAMDA','adam','10aad',new Date())
+const Hospital5 = new Hospital(1, 'HAMDA','adam','10aad',new Date())
+const Hospital6 = new Hospital(1, 'HAMDA','adam','10aad',new Date())
+const Hospital7 = new Hospital(1, 'HAMDA','adam','10aad',new Date())
+const Hospital8 = new Hospital(1, 'HAMDA','adam','10aad',new Date())
+const Hospital9 = new Hospital(1, 'HAMDA','adam','10aad',new Date())
+console.log(Hospital1,Hospita2,Hospital3,Hospita3,Hospital4,Hospital5,Hospital6,Hospital7,Hospital8)
+
