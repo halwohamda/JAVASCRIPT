4d3a453eee1307c79f53a9540089d16362b393c8
halwohamda <halwohamda@gmail.com>
2024-02-07 06:18:35 -0800

    done

--- Advanced/class.js
+++ Advanced/class.js
@@ -2,5 +2,12 @@ class Company {
     constructor(id,firstName,LastName){
         this.id = id;
         this.firstName = firstName;
+        this.LastName = LastName;
+
     }
+}
+
+
+getCompany(){
+    return``
 }
