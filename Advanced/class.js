1510e390500d16ef82f37cfb0d63b7784d25599e
halwohamda <halwohamda@gmail.com>
2024-02-07 06:30:32 -0800

    done

--- Advanced/class.js
+++ Advanced/class.js
@@ -1,5 +1,5 @@
-class Company {
-    constructor(id,firstName,LastName){
+ export class Company {
+ constructor(id,firstName,LastName){
         this.id = id;
         this.firstName = firstName;
         this.LastName = LastName;
