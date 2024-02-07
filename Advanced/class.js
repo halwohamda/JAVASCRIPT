caa2dcfce77ca44d922064d31d83c5d6b2523bf8
halwohamda <halwohamda@gmail.com>
2024-02-07 06:25:32 -0800

    done

--- Advanced/class.js
+++ Advanced/class.js
@@ -20,5 +20,5 @@ const Company6 = new Company (1,'hamda','abdi',)
 const Company7 = new Company (1,'hamda','abdi',)
 
 
-console.log(Company1)
+console.log(Company1,Company2,Company3,Company4,Company5,Company6,Company7)
 
