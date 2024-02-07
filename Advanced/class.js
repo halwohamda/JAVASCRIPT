5208fb826cb985a40ed105194a1ce5b3bda4a10a
halwohamda <halwohamda@gmail.com>
2024-02-07 06:27:36 -0800

    done

--- Advanced/class.js
+++ Advanced/class.js
@@ -11,13 +11,13 @@ class Company {
     }
 }
 
-const Company1 = new Company (1,'hamda','abdi',);
-const Company2 = new Company (1,'hamda','abdi',);
-const Company3 = new Company (1,'hamda','abdi',);
-const Company4 = new Company (1,'hamda','abdi',);
-const Company5 = new Company (1,'hamda','abdi',);
-const Company6 = new Company (1,'hamda','abdi',);
-const Company7 = new Company (1,'hamda','abdi',);
+const Company1 = new Company (1,'hamda','abdi',)
+const Company2 = new Company (1,'hamda','abdi',)
+const Company3 = new Company (1,'hamda','abdi',)
+const Company4 = new Company (1,'hamda','abdi',)
+const Company5 = new Company (1,'hamda','abdi',)
+const Company6 = new Company (1,'hamda','abdi',)
+const Company7 = new Company (1,'hamda','abdi',)
 
 
 console.log(Company1,Company2,Company3,Company4,Company5,Company6,Company7)
