16f9b680356a45b3a9a172a368c4ed800aeb6477
halwohamda <halwohamda@gmail.com>
2024-02-07 07:09:38 -0800

    done

--- Advanced/ex.js
+++ Advanced/ex.js
@@ -3,9 +3,9 @@ const Hospital1 = new Hospital(1, 'HAMDA','adam','10aad',new Date())
 const Hospital2 = new Hospital(1, 'HAMDA','adam','10aad',new Date())
 const Hospital3 = new Hospital(1, 'HAMDA','adam','10aad',new Date())
 const Hospital4 = new Hospital(1, 'HAMDA','adam','10aad',new Date())
-const Hospital5 = new Hospital(1, 'HAMDA','adam','10aad',new Date());
-const Hospital6 = new Hospital(1, 'HAMDA','adam','10aad',new Date());
-const Hospital7 = new Hospital(1, 'HAMDA','adam','10aad',new Date());
+const Hospital5 = new Hospital(1, 'HAMDA','adam','10aad',new Date())
+const Hospital6 = new Hospital(1, 'HAMDA','adam','10aad',new Date())
+const Hospital7 = new Hospital(1, 'HAMDA','adam','10aad',new Date())
 const Hospital8 = new Hospital(1, 'HAMDA','adam','10aad',new Date())
 const Hospital9 = new Hospital(1, 'HAMDA','adam','10aad',new Date())
 console.log(Hospital1,Hospital2,Hospital3,Hospital3,Hospital4,Hospital5,Hospital6,Hospital7,Hospital8)
