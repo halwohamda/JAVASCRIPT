60747b0e774aff25e028743fd0e8cfbf762d2ed4
halwohamda <halwohamda@gmail.com>
2024-02-09 03:35:11 -0800

    done

--- Advanced/ES6.js
+++ Advanced/ES6.js
@@ -1,6 +1,6 @@
 const numbers = [1,3,4,5,]
 const filternumbers = numbers.filter(numbers =>{
     const natijo = numbers <=1? "haa":"maya";
-    console.log(natijo)
+    // console.log(natijo)
 })
 
