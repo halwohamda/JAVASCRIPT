3af03ec30806bdd399e4586d800bdee2c63aea46
halwohamda <halwohamda@gmail.com>
2024-02-09 01:44:22 -0800

    done

--- Advanced/ES6.js
+++ Advanced/ES6.js
@@ -1,5 +1,5 @@
 // const numbers = [22,33,44,55,66,77,88,99,100]
 // const numbersfilter = numbers.filter (numbers =>{
-//     const result = numbers <=55? "hahey":"mayey";
-//     console.log(result)
-// }) 
+    const result = numbers <=55? "hahey":"mayey";
+    console.log(result)
+}) 
