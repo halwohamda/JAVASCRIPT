7693534eb2c6b0f01cc3b32c3cd5c7b8cf1087c7
halwohamda <halwohamda@gmail.com>
2024-02-12 19:56:21 -0800

    done

--- fech_async_await/index.js
+++ fech_async_await/index.js
@@ -2,3 +2,7 @@ let p = document.querySelector('p');
 let spn = document.querySelector('span');
 let pre = document.querySelector('pre');
 let div = document.querySelector('div');
+
+const getData = async ()=>{
+    const response = await fetch('')
+}
