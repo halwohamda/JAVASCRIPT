8a9cefec0cd750c7edbcc62ef5a167b755a9b608
halwohamda <halwohamda@gmail.com>
2024-02-13 20:04:27 -0800

    Done

--- fech_async_await/index.js
+++ fech_async_await/index.js
@@ -1,17 +1,1 @@
 let p = document.querySelector('p');
-let span = document.querySelector('p');
-let pre = document.querySelector('pre');
-let div = document.querySelector('div');
-
-const getData = async()=>{
-    const yes = await fetch('https://jsonplaceholder.typicode.com/posts')
-    .then((yes)=>yes.json())
-    .then((no)=>{
-        no.forEach((user)=>{
-           const display = `<p>userId:${user.id}</p> <span>userTitle:${user.title}</span> <pre>userTitle:${user.body}</pre>`
-           div.insertAdjacentHTML('beforeend', display);
-        })
-        
-    })
-}
-getData();
