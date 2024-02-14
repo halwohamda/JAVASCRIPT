2d5bc33140ae8787b925111ce76a3832694d207b
halwohamda <halwohamda@gmail.com>
2024-02-13 19:48:40 -0800

    Done

--- fech_async_await/index.js
+++ fech_async_await/index.js
@@ -3,8 +3,10 @@ let span = document.querySelector('p');
 let pre = document.querySelector('pre');
 let div = document.querySelector('div');
 
-
-const getData = async ()=>{
-    const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-    
+const getData = async()=>{
+    const yes = await fetch('https://jsonplaceholder.typicode.com/posts/')
+    .then((yes)=>yes.json());
+    .then((no)=>{
+        
+    })
 }
